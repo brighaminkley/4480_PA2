53e089a0723ff6efb5a01e050a0a530fde807964
brighaminkley <brig@ratenow.net>
2025-03-10 19:26:30 -0600

    Got the ethernet has no ARP error again.

--- Brigham_Inkley_u0887324.py
+++ Brigham_Inkley_u0887324.py
@@ -13,80 +13,83 @@ VIRTUAL_IP = IPAddr("10.0.0.10")
 SERVERS = [IPAddr("10.0.0.5"), IPAddr("10.0.0.6")]
 MACS = {IPAddr("10.0.0.5"): EthAddr("00:00:00:00:00:05"),
         IPAddr("10.0.0.6"): EthAddr("00:00:00:00:00:06")}
+server_index = 0  # Tracks which server to assign next
 
-class LoadBalancer(object):
+class LoadBalancer (object):
     def __init__(self, connection):
         self.connection = connection
-        self.server_index = 0  # Make server_index an instance variable
         connection.addListeners(self)
 
     def _handle_PacketIn(self, event):
         try:
+            global server_index
             packet = event.parsed
 
-            # Check for ARP request for the virtual IP
+            # Check if the packet is an ARP request for the virtual IP
             if packet.type == packet.ARP_TYPE and packet.payload.protodst == VIRTUAL_IP:
                 log.info("Intercepted ARP request for virtual IP")
-                server_ip = SERVERS[self.server_index]
+                
+                # Round-robin to choose which server to respond with
+                server_ip = SERVERS[server_index]
                 server_mac = MACS[server_ip]
 
-                # Create and send ARP reply
+                # Create ARP reply
                 arp_reply = arp()
                 arp_reply.hwsrc = server_mac
-                arp_reply.hwdst = packet.payload.protosrc
+                arp_reply.hwdst = packet.payload.protosrc  # Source MAC address of the ARP request
                 arp_reply.opcode = arp.REPLY
                 arp_reply.protosrc = VIRTUAL_IP
-                arp_reply.protodst = packet.payload.protosrc
+                arp_reply.protodst = packet.payload.protosrc  # Source IP address of the ARP request
 
+                # Wrap the ARP reply in an Ethernet frame
                 ethernet_reply = ethernet()
                 ethernet_reply.src = server_mac
                 ethernet_reply.dst = packet.src
                 ethernet_reply.type = ethernet.ARP_TYPE
                 ethernet_reply.payload = arp_reply
 
+                # Send ARP reply
                 msg = of.ofp_packet_out()
                 msg.data = ethernet_reply.pack()
                 msg.actions.append(of.ofp_action_output(port=event.port))
                 self.connection.send(msg)
 
                 # Rotate server for round-robin balancing
-                self.server_index = (self.server_index + 1) % len(SERVERS)
+                server_index = (server_index + 1) % len(SERVERS)
 
-                # Install flow for this client-server interaction
-                self.install_flow(event, server_ip, server_mac, packet)
-
+                # Install client-to-server and server-to-client flows
+                self.install_flow(event.port, server_ip, server_mac, packet)
+        
         except Exception as e:
             log.error(f"Error handling PacketIn event: {e}")
+        
 
-    def install_flow(self, event, server_ip, server_mac, packet):
-        client_port = event.port  # Now using the passed event's port
-
+    def install_flow(self, client_port, server_ip, server_mac, packet):
+        # Install client-to-server flow
         log.info("Installing client-to-server flow:")
         log.info(f"  Match: in_port={client_port}, dl_type=0x0800, nw_dst={VIRTUAL_IP}")
-        log.info(f"  Actions: set_dst(mac)={server_mac}, set_dst(ip)={server_ip}, output={event.port}")
+        log.info(f"  Actions: set_dst(mac)={server_mac}, set_dst(ip)={server_ip}, output={self.connection.ports[server_ip]}")
 
-        # Install flow for traffic from client to server
         msg_client_to_server = of.ofp_flow_mod()
-        msg_client_to_server.match.dl_type = 0x0800  # IPv4
-        msg_client_to_server.match.nw_dst = VIRTUAL_IP  # Match virtual IP
-        msg_client_to_server.match.in_port = client_port
+        msg_client_to_server.match.dl_type = 0x0800  # Match IPv4 traffic
+        msg_client_to_server.match.nw_dst = VIRTUAL_IP  # Match the virtual IP
+        msg_client_to_server.match.in_port = client_port  # Match client port
 
         msg_client_to_server.actions.append(of.ofp_action_dl_addr.set_dst(server_mac))
         msg_client_to_server.actions.append(of.ofp_action_nw_addr.set_dst(server_ip))
-        msg_client_to_server.actions.append(of.ofp_action_output(port=event.port))  # Use event.port here
+        msg_client_to_server.actions.append(of.ofp_action_output(port=self.connection.ports[server_ip]))
         self.connection.send(msg_client_to_server)
 
+        # Install server-to-client flow
         log.info("Installing server-to-client flow:")
         log.info(f"  Match: in_port={self.connection.ports[server_ip]}, dl_type=0x0800, nw_src={server_ip}, nw_dst={packet.payload.protosrc}")
         log.info(f"  Actions: set_src(ip)={VIRTUAL_IP}, set_src(mac)={server_mac}, output={client_port}")
 
-        # Install flow for traffic from server to client
         msg_server_to_client = of.ofp_flow_mod()
-        msg_server_to_client.match.dl_type = 0x0800  # IPv4
-        msg_server_to_client.match.nw_src = server_ip  # Match server IP
-        msg_server_to_client.match.nw_dst = packet.payload.protosrc
-
-        msg_server_to_client.match.in_port = self.connection.ports[server_ip]
+        msg_server_to_client.match.dl_type = 0x0800  # Match IPv4 traffic
+        msg_server_to_client.match.nw_src = server_ip  # Match the server's IP
+        msg_server_to_client.match.nw_dst = packet.payload.protosrc  # Match the client's IP
+        msg_server_to_client.match.in_port = self.connection.ports[server_ip]  # Match server port
 
         msg_server_to_client.actions.append(of.ofp_action_nw_addr.set_src(VIRTUAL_IP))
         msg_server_to_client.actions.append(of.ofp_action_dl_addr.set_src(server_mac))
