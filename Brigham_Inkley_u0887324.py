5a901cabec46c27bc40e244e0032e751147d24dd
brighaminkley <brig@ratenow.net>
2025-03-11 17:29:18 -0600

    Fixed unsignedChar error I think?

--- Brigham_Inkley_u0887324.py
+++ Brigham_Inkley_u0887324.py
@@ -65,11 +65,12 @@ class LoadBalancer(object):
                 log.info(f"Sent ARP reply with MAC {server_mac} for virtual IP {VIRTUAL_IP}")
 
                 # Install client-to-server flow
-                self.install_flow_rules(event.port, packet.src, server_ip, server_mac, server_port)
+                client_ip = packet.payload.protosrc
+                self.install_flow_rules(event.port, packet.src, client_ip, server_ip, server_mac, server_port)
 
-    def install_flow_rules(self, client_port, client_mac, server_ip, server_mac, server_port):
+    def install_flow_rules(self, client_port, client_mac, client_ip, server_ip, server_mac, server_port):
         """
-        Install flow rules to handle traffic between clients and servers.
+        Install flow rules for client-to-server and server-to-client communication.
         """
 
         # Client-to-server flow
@@ -78,10 +79,11 @@ class LoadBalancer(object):
         match.dl_type = 0x0800  # IP
         match.nw_dst = VIRTUAL_IP
 
-        actions = []
-        actions.append(of.ofp_action_dl_addr.set_dst(server_mac))
-        actions.append(of.ofp_action_nw_addr.set_dst(server_ip))
-        actions.append(of.ofp_action_output(port=server_port))
+        actions = [
+            of.ofp_action_dl_addr.set_dst(server_mac),
+            of.ofp_action_nw_addr.set_dst(server_ip),
+            of.ofp_action_output(port=server_port)
+        ]
 
         msg = of.ofp_flow_mod()
         msg.match = match
@@ -94,19 +96,21 @@ class LoadBalancer(object):
         match.in_port = server_port
         match.dl_type = 0x0800  # IP
         match.nw_src = server_ip
-        match.nw_dst = client_mac  # Fix: Use actual client IP here
+        match.nw_dst = client_ip  # FIXED: Use the client's IP, not MAC
 
-        actions = []
-        actions.append(of.ofp_action_dl_addr.set_src(server_mac))
-        actions.append(of.ofp_action_nw_addr.set_src(VIRTUAL_IP))
-        actions.append(of.ofp_action_dl_addr.set_dst(client_mac))
-        actions.append(of.ofp_action_output(port=client_port))
+        actions = [
+            of.ofp_action_dl_addr.set_src(server_mac),
+            of.ofp_action_nw_addr.set_src(VIRTUAL_IP),
+            of.ofp_action_dl_addr.set_dst(client_mac),
+            of.ofp_action_output(port=client_port)
+        ]
 
         msg = of.ofp_flow_mod()
         msg.match = match
         msg.actions = actions
         self.connection.send(msg)
-        log.info(f"Installed server-to-client flow: server {server_ip} -> client {client_mac}.")
+        log.info(f"Installed server-to-client flow: server {server_ip} -> client IP {client_ip}.")
+
 
 def launch():
     def start_switch(event):
