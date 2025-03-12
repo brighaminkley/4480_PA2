168465d4e02afa427728f67347745c1aaecdefaa
brighaminkley <brig@ratenow.net>
2025-03-12 14:07:38 -0600

    back again.

--- Brigham_Inkley_u0887324.py
+++ Brigham_Inkley_u0887324.py
@@ -3,8 +3,8 @@
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.addresses import EthAddr, IPAddr
-from pox.lib.packet.arp import arp, ethernet
+from pox.lib.addresses import EthAddr
+from pox.lib.packet import ethernet
 
 VIRTUAL_IP = "10.0.0.10"
 SERVER1_IP = "10.0.0.5"
@@ -12,115 +12,44 @@ SERVER2_IP = "10.0.0.6"
 SERVER1_MAC = "00:00:00:00:00:05"
 SERVER2_MAC = "00:00:00:00:00:06"
 
-use_server1 = True  # Start with server 1
-
-
 def launch():
     def _handle_ConnectionUp(event):
-        global use_server1
-
         for m in event.connection.features.ports:
             if m.port_no < of.OFPP_MAX:
                 print(m.port_no, m.name)
 
-        # Initial flow rules (if any) can be added here
-
-
     def _handle_PacketIn(event):
         packet = event.parsed
-        core.getLogger().info(f"PacketIn event: type={packet.type}, src={packet.src}, dst={packet.dst}, port={event.port}")
-
-        if packet.type == ethernet.ARP_TYPE:
-            handle_arp(packet, event.port, event.connection)
-        elif packet.type == 0x0800: # Only handle IPv4 packets
-            core.getLogger().info(
-                f"Received IPv4 packet: type={packet.type}, src={packet.src}, dst={packet.dst}, in_port={event.port}")
-            # Handle IPv4 traffic here if needed in the future
-        else:
-            core.getLogger().info(f"Ignoring packet type: {packet.type}")
-            return
-
-
-    def handle_arp(packet, port, event_connection):
-        global use_server1
-        a = packet.payload
-        if a.opcode == arp.REQUEST:  # Check if it's an ARP request
-            if a.protodst == IPAddr(VIRTUAL_IP):  # ARP request for virtual IP
-                target_mac = SERVER1_MAC if use_server1 else SERVER2_MAC
-                target_ip = SERVER1_IP if use_server1 else SERVER2_IP
-
-                r = arp()
-                r.hwtype = a.hwtype
-                r.prototype = a.prototype
-                r.hwlen = a.hwlen
-                r.protolen = a.protolen
-                r.opcode = arp.REPLY
-                r.hwdst = packet.src
-                r.hwsrc = EthAddr(target_mac)
-                r.protodst = a.protosrc
-                r.protosrc = IPAddr(VIRTUAL_IP)
-
-                e = ethernet()
-                e.type = ethernet.ARP_TYPE
-                e.src = EthAddr(target_mac)
-                e.dst = packet.src
-                e.payload = r
-
-                # Send ARP reply out the correct port
-                msg = of.ofp_packet_out()
-                msg.data = e.pack()
-                msg.actions.append(of.ofp_action_output(port=port))
-                event_connection.send(msg)
-
-                core.getLogger().info(
-                    f"Sent ARP reply to {a.protosrc} with MAC {target_mac} via port {port}")
-
-                # Install flows
-                install_flow_rules(event_connection, port, target_ip)
-
-                # Toggle between servers
-                use_server1 = not use_server1
-
-
-
-    def install_flow_rules(event_connection, port, target_ip):
-        target_mac = SERVER1_MAC if target_ip == SERVER1_IP else SERVER2_MAC
-
-        # Client -> Server flow (by IP, not hardcoded MAC)
-        match_client_to_server = of.ofp_match()
-        match_client_to_server.in_port = port
-        match_client_to_server.nw_dst = IPAddr(target_ip)
-        match_client_to_server.dl_type = 0x0800  # IPv4
-
-        action_client_to_server = of.ofp_action_output(port=5 if target_ip == SERVER1_IP else 6)
-        flow_mod_client_to_server = of.ofp_flow_mod()
-        flow_mod_client_to_server.match = match_client_to_server
-        flow_mod_client_to_server.actions.append(action_client_to_server)
-        event_connection.send(flow_mod_client_to_server)
-
-        core.getLogger().info(f"Installed flow: Client -> Server {target_ip} via port {port}")
-
-        # Server -> Client flow (return traffic)
-        match_server_to_client = of.ofp_match()
-        match_server_to_client.in_port = 5 if target_ip == SERVER1_IP else 6
-        match_server_to_client.nw_src = IPAddr(target_ip)
-        match_server_to_client.nw_dst = IPAddr("10.0.0.1")
-        match_server_to_client.dl_type = 0x0800  # IPv4
-
-        action_server_to_client = of.ofp_action_output(port=port)
-        flow_mod_server_to_client = of.ofp_flow_mod()
-        flow_mod_server_to_client.match = match_server_to_client
-        flow_mod_server_to_client.actions.append(action_server_to_client)
-        event_connection.send(flow_mod_server_to_client)
-
-        core.getLogger().info(f"Installed flow: Server {target_ip} -> Client via port {port}")
-
-
-
+        # Log incoming packet details
+        core.getLogger().info(
+            f"Received packet: type={packet.type}, src={packet.src}, dst={packet.dst}, in_port={event.port}")
+        # Only handle Ethernet packets
+        if packet.type == ethernet.ETHER_TYPE:
+            dst_mac = packet.dst
+            in_port = event.port
+            event_connection = event.connection
+            if dst_mac == EthAddr(SERVER1_MAC):
+                install_flow_rules(event_connection, in_port, SERVER1_MAC, 5)  # 5 is the port for h5
+            elif dst_mac == EthAddr(SERVER2_MAC):
+                install_flow_rules(event_connection, in_port, SERVER2_MAC, 6)  # 6 is the port for h6
+            elif dst_mac == packet.src:
+                if packet.src == EthAddr(SERVER1_MAC):
+                    install_flow_rules(event_connection, in_port, packet.src, 1)
+                elif packet.src == EthAddr(SERVER2_MAC):
+                    install_flow_rules(event_connection, in_port, packet.src, 1)
+    def install_flow_rules(event_connection, in_port, dst_mac, out_port):
+        core.getLogger().info(f"Installing flow rule: in_port={in_port}, dst_mac={dst_mac}, out_port={out_port}")
+        match = of.ofp_match()
+        match.in_port = in_port
+        match.dl_dst = EthAddr(dst_mac)
+        flow_mod = of.ofp_flow_mod()
+        flow_mod.match = match
+        action = of.ofp_action_output(port=out_port)
+        flow_mod.actions.append(action)
+        event_connection.send(flow_mod)
+        core.getLogger().info(f"flow mod sent: {flow_mod}")
     core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
     core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
-
-
 launch()
 
 # from pox.core import core
